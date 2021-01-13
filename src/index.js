a568db6dd4a64049edecd2af628f8d5804c8e0c4
carla-isa <72775352+carla-isa@users.noreply.github.com>
2021-01-13 12:01:21 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react" 
-import ReactDOM from "react-dom"
-import App from "./App"
-import "./estilos.css"
-
-
-
-ReactDOM.render( <App/> ,document.getElementById("root")) /*El primer elemento tiene que ser algo que pueda mostrar en pantalla*/
-
